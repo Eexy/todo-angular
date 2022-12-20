4590e733abb3d6ec0d811639226f6146802d17a8
eexy <eexdhe@gmail.com>
2022-12-20 18:03:55 +0100

    feat(component): create todo list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 import { TodoComponent } from './components/todo/todo.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoFormComponent, TodoComponent],
+  declarations: [AppComponent, TodoFormComponent, TodoComponent, TodoListComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/todo-list/todo-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { TodoService } from 'src/app/services/todo/todo.service';
+import { Todo } from 'src/app/types/todo';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css'],
+})
+export class TodoListComponent {
+  todos: Todo[] = [];
+
+  constructor(private todoServices: TodoService) {}
+
+  ngOnInit() {
+    this.getTodos();
+  }
+
+  getTodos(): void {
+    this.todoServices.getTodos().subscribe((todos) => (this.todos = todos));
+  }
+}
