3072107ef3f9b3a3f56125296d3a11012407bbc7
eexy <eexdhe@gmail.com>
2022-12-21 17:58:40 +0100

    refactor: delete unused style files

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -5,7 +5,6 @@ import { Todo } from 'src/app/types/todo';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
   todos: Todo[] = [];
--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -5,7 +5,6 @@ import { Todo } from 'src/app/types/todo';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent {
   @Input() todo?: Todo;
